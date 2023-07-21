32c69591b86a0b44a1d105973f62798713ae21a4
Laisson Bruno <laissontkd@hotmail.com>
2023-07-20 23:32:01 -0300

    paginacao

--- API/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ API/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -10,13 +10,30 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody]Filme filme)
-    {
-        filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
-
+    public IActionResult AdicionaFilme([FromBody]Filme filme)
+    {        
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmeID), new { id = filme.Id }, filme);
+        }
     }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmeID(int id)
+    {
+       var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok();
+    }
+
 }
